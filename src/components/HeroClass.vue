48c3d6317ccae38c1874c3e0aac2479eb6256170
bbricard <baptiste.bricard@etudiant.univ-rennes1.fr>
2025-01-21 16:30:47 +0100

    ➕ Add Hero class

---
+++ src/components/HeroClass.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+    required: true,
+  },
+  rank: {
+    type: Number,
+    required: true,
+  },
+  branch: {
+    type: Number,
+    required: false,
+  },
+  specialize: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <p>
+      <span>{{ props.class }}</span
+      ><span v-if="specialize">/ {{ specialize }}</span>
+    </p>
+    <span>rank:{{}}</span>
+  </div>
+</template>
