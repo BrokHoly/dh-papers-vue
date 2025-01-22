313f33d45a79959aa872fe1aaff06121f63a2a1a
bbricard <baptiste.bricard@etudiant.univ-rennes1.fr>
2025-01-22 15:27:45 +0100

    HeroNames out of Profile

---
+++ src/components/HeroNames.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { ProfileData } from '@/interfaces/interfaces'
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  profile: {
+    type: Object as PropType<ProfileData>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.profile.heroName }}</h1>
+    <p>{{ props.profile.playerName }}</p>
+  </div>
+</template>
+
+<style></style>
